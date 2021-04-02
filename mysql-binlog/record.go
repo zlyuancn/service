bd1d3619253213ef7695788ded92a69c49e10843
zlyuancn <zlyuancn@163.com>
2021-04-02 10:08:48 +0800

    为Record添加解析方法

--- mysql-binlog/record.go
+++ mysql-binlog/record.go
@@ -21,15 +21,47 @@ type Record struct {
 	Timestamp uint32                 `json:"timestamp"`
 }
 
+// 获取old数据的json输出
 func (r *Record) OldString() string {
 	text, _ := jsoniter.MarshalToString(r.Old)
 	return text
 }
+
+// 获取new数据的json输出
 func (r *Record) NewString() string {
 	text, _ := jsoniter.MarshalToString(r.New)
 	return text
 }
+
+// 将record的所有数据转为json
 func (r *Record) String() string {
 	text, _ := jsoniter.MarshalToString(r)
 	return text
+}
+
+// 将record的所有数据解析到a
+func (r *Record) Unmarshal(a interface{}) error {
+	bs, err := jsoniter.Marshal(r)
+	if err != nil {
+		return err
+	}
+	return jsoniter.Unmarshal(bs, a)
+}
+
+// 将old数据解析到a
+func (r *Record) UnmarshalOld(a interface{}) error {
+	bs, err := jsoniter.Marshal(r.Old)
+	if err != nil {
+		return err
+	}
+	return jsoniter.Unmarshal(bs, a)
+}
+
+// 将new数据解析到a
+func (r *Record) UnmarshalNew(a interface{}) error {
+	bs, err := jsoniter.Marshal(r.New)
+	if err != nil {
+		return err
+	}
+	return jsoniter.Unmarshal(bs, a)
 }
