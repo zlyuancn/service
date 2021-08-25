8a21eeb8f7595057a4b8e1f66539cd311709362d
zlyuancn <zlyuancn@163.com>
2021-08-25 17:38:45 +0800

    添加一些配置项

--- api/config/config.go
+++ api/config/config.go
@@ -10,13 +10,39 @@ package config
 
 var Conf Config
 
+const (
+	// 默认bind
+	defaultBind = ":8080"
+	// 默认post允许最大数据大小
+	defaultPostMaxMemory = 32 << 20
+	// 将header中第一个有效的地址视为客户端地址
+	defaultUseFirstValidRemoteAddrOfHeaders = true
+)
+
 // api服务配置
 type Config struct {
-	Bind                          string // bind地址
-	IPWithNginxForwarded          bool   // 适配nginx的Forwarded获取ip, 优先级高于nginx的Real
-	IPWithNginxReal               bool   // 适配nginx的Real获取ip, 优先级高于sock连接的ip
-	LogApiResultInDevelop         bool   // 在开发环境中输出api结果
-	SendDetailedErrorInProduction bool   // 在生产环境发送详细的错误到客户端
-	AlwaysLogHeaders              bool   // 总是输出headers日志, 如果设为false, 只会在出现错误时才会输出headers日志
-	AlwaysLogBody                 bool   // 总是输出body日志, 如果设为false, 只会在出现错误时才会输出body日志
+	Bind                             string // bind地址
+	IPWithNginxForwarded             bool   // 适配nginx的Forwarded获取ip, 优先级高于nginx的Real
+	IPWithNginxReal                  bool   // 适配nginx的Real获取ip, 优先级高于sock连接的ip
+	LogApiResultInDevelop            bool   // 在开发环境中输出api结果
+	SendDetailedErrorInProduction    bool   // 在生产环境发送详细的错误到客户端
+	AlwaysLogHeaders                 bool   // 总是输出headers日志, 如果设为false, 只会在出现错误时才会输出headers日志
+	AlwaysLogBody                    bool   // 总是输出body日志, 如果设为false, 只会在出现错误时才会输出body日志
+	UseFirstValidRemoteAddrOfHeaders bool   // 将header中第一个有效的地址视为客户端地址
+	PostMaxMemory                    int64  // post允许客户端传输最大数据大小, 单位字节
+}
+
+func NewConfig() *Config {
+	return &Config{
+		UseFirstValidRemoteAddrOfHeaders: defaultUseFirstValidRemoteAddrOfHeaders,
+	}
+}
+
+func (conf *Config) Check() {
+	if conf.Bind == "" {
+		conf.Bind = defaultBind
+	}
+	if conf.PostMaxMemory <= 0 {
+		conf.PostMaxMemory = defaultPostMaxMemory
+	}
 }
--- api/service.go
+++ api/service.go
@@ -67,12 +67,13 @@ func (a *ApiService) Inject(sc ...interface{}) {
 }
 
 func (a *ApiService) Start() error {
-	var conf config.Config
-	err := a.app.GetConfig().ParseServiceConfig(nowServiceType, &conf)
+	conf := config.NewConfig()
+	err := a.app.GetConfig().ParseServiceConfig(nowServiceType, conf)
 	if err != nil {
 		return err
 	}
-	config.Conf = conf
+	conf.Check()
+	config.Conf = *conf
 
 	a.app.Debug("正在启动api服务", zap.String("bind", conf.Bind))
 	opts := []iris.Configurator{
@@ -81,6 +82,9 @@ func (a *ApiService) Start() error {
 		iris.WithOptimizations,                 // 启用性能优化
 		iris.WithoutStartupLog,                 // 不要打印iris启动信息
 		iris.WithPathEscape,                    // 解析path转义
+		iris.WithFireMethodNotAllowed,          // 路由未找到时返回405而不是404
+		iris.WithConfiguration(iris.Configuration{RemoteAddrHeadersForce: conf.UseFirstValidRemoteAddrOfHeaders}),
+		iris.WithPostMaxMemory(conf.PostMaxMemory), // post允许客户端传输最大数据大小
 	}
 	if conf.IPWithNginxForwarded {
 		opts = append(opts, iris.WithRemoteAddrHeader("X-Forwarded-For"))
