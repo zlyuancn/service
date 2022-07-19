eea50ecd6c7bf3f09e8ccb85abee528de2525c43
zlyuanzhang <zlyuanzhang@tencent.com>
2022-07-19 21:03:11 +0800

    优化默认配置值

--- api/config/config.go
+++ api/config/config.go
@@ -11,10 +11,26 @@ package config
 const (
 	// 默认bind
 	defaultBind = ":8080"
+	// 请求日志等级设为info
+	defReqLogLevelIsInfo = true
+	// 响应日志等级设为info
+	defRspLogLevelIsInfo = true
+	// bind日志等级设为info
+	defBindLogLevelIsInfo = true
 	// 默认适配nginx的Forwarded获取ip
 	defaultIPWithNginxForwarded = true
 	// 默认适配nginx的Real获取ip
 	defaultIPWithNginxReal = true
+	// 在开发环境中输出api结果
+	defLogApiResultInDevelop = true
+	// 在生产环境中输出api结果
+	defLogApiResultInProd = false
+	// 在生产环境发送详细的错误到客户端
+	defSendDetailedErrorInProduction = false
+	// 总是输出headers日志
+	defAlwaysLogHeaders = true
+	// 总是输出body日志
+	defAlwaysLogBody = true
 	// 默认输出结果最大大小
 	defaultLogApiResultMaxSize = 10240
 	// 输出body最大大小
@@ -33,18 +49,27 @@ type Config struct {
 	IPWithNginxReal               bool   // 适配nginx的Real获取ip, 优先级高于sock连接的ip
 	LogApiResultInDevelop         bool   // 在开发环境中输出api结果
 	LogApiResultInProd            bool   // 在生产环境中输出api结果
-	LogApiResultMaxSize           int    // 输出结果最大大小
 	SendDetailedErrorInProduction bool   // 在生产环境发送详细的错误到客户端
 	AlwaysLogHeaders              bool   // 总是输出headers日志, 如果设为false, 只会在出现错误时才会输出headers日志
 	AlwaysLogBody                 bool   // 总是输出body日志, 如果设为false, 只会在出现错误时才会输出body日志
+	LogApiResultMaxSize           int    // 输出结果最大大小
 	LogBodyMaxSize                int64  // 输出body最大大小
 	PostMaxMemory                 int64  // post允许客户端传输最大数据大小, 单位字节
 }
 
 func NewConfig() *Config {
 	return &Config{
-		IPWithNginxForwarded: defaultIPWithNginxForwarded,
-		IPWithNginxReal:      defaultIPWithNginxReal,
+		Bind:                          defaultBind,
+		ReqLogLevelIsInfo:             defReqLogLevelIsInfo,
+		RspLogLevelIsInfo:             defRspLogLevelIsInfo,
+		BindLogLevelIsInfo:            defBindLogLevelIsInfo,
+		IPWithNginxForwarded:          defaultIPWithNginxForwarded,
+		IPWithNginxReal:               defaultIPWithNginxReal,
+		LogApiResultInDevelop:         defLogApiResultInDevelop,
+		LogApiResultInProd:            defLogApiResultInProd,
+		SendDetailedErrorInProduction: defSendDetailedErrorInProduction,
+		AlwaysLogHeaders:              defAlwaysLogHeaders,
+		AlwaysLogBody:                 defAlwaysLogBody,
 	}
 }
 
